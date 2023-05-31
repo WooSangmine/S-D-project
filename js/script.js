265f9dc40d373c449051980fcd150db0c8477a76
woosangmine <2021232@sungil-i.kr>
2023-05-31 18:26:28 +0900

    123123
    123123

--- js/script.js
+++ js/script.js
@@ -29,11 +29,31 @@ jQuery(document).ready(function() {
         if($('#menuicon').is(":checked") == true){
             $('.hgtap').animate({
                 marginRight:0
-            },1000);};
+            },500);};
             if($('#menuicon').is(":checked") == false){
                 $('.hgtap').animate({
                     marginRight:-300
-                },1000);};
+                },500);};
     });
 
+    $(function() {
+        $("input[type=submit]").click(function() {
+            var result = $.trim($("input[type=text]").val());
+            if (!result) {
+              alert("입력하십시오");
+            }
+        })
+    })
+
+    $(function() {
+        $(".hgtap> ul>li ").click(function() {
+            alert("로그인을 하세요")
+        });
+    });
+
+    $(function() {
+        $(".hgtap> li ").click(function() {
+            location.href = "login.php";
+        });
+    });
 });
